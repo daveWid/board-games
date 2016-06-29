7b6598d4b47708326b65156b5b28bd15f008634c
Dave Widmer <dave@davewidmer.net>
2016-06-29 14:55:51 -0500

    Rename mechanics to tags in the game data

--- data/games.js
+++ data/games.js
@@ -6,7 +6,7 @@ export default [{
   playTimeQuick: 30,
   playTimeLong: 60,
   rulesPDF: 'https://cdn1.daysofwonder.com/tickettoride/en/img/tt_rules_2015_en.pdf',
-  mechanics: ['Card Drafting', 'Hand Management', 'Set Collection', 'Route/Network Building'],
+  tags: ['Card Drafting', 'Hand Management', 'Set Collection', 'Route/Network Building'],
   owner: 'Dave'
 }, {
   name: '1830: Railways & Robber Barons',
@@ -16,7 +16,77 @@ export default [{
   playTimeQuick: 180,
   playTimeLong: 300,
   rulesPDF: 'http://www.mayfairgames.com/files/supportfiles/mfg1830-rules-v1_0.pdf',
-  mechanics: ['Auction/Bidding', 'Route/Network Building', 'Stock Holding', 'Tile Placement'],
+  tags: ['Auction/Bidding', 'Route/Network Building', 'Stock Holding', 'Tile Placement'],
+  owner: 'Dave'
+}, {
+  name: 'Castles of Burgundy',
+  description: 'The Hundred Years’ war is over and the Renaissance is looming. Conditions are perfect for the princes of the Loire Valley to propel their estates to prosperity and prominence. Through strategic trading and building, clever planning, and careful thought, players add settlements and castles, practice trade along the river, exploit silver mines, farm livestock, and more in this classic Stefan Feld Eurogame.',
+  playersMin: 2,
+  playersMax: 4,
+  playTimeQuick: 30,
+  playTimeLong: 90,
+  rulesPDF: 'https://www.ravensburger.com/content/wcm/mediadata/pdf/Microsites/Alea/Die%20Burgen%20von%20Burgund/Burgund_Englisch.pdf',
+  tags: ['Dice Rolling', 'Set Collection', 'Tile Placement'],
+  owner: 'Dave'
+}, {
+  name: 'Carcassonne',
+  description: 'In Carcassonne, players build the area surrounding this impressive city, one tile at a time. They then place a follower on fields, cities, roads or monasteries in order to score as many points as possible. These followers will become knights, monks, farmers and thieves, depending on where they are placed. No matter their function, the player who will most cleverly use their followers will win the game.',
+  playersMin: 2,
+  playersMax: 5,
+  playTimeQuick: 30,
+  playTimeLong: 45,
+  rulesPDF: 'http://www.zmangames.com/uploads/4/7/1/7/47170931/carcassonne_new_edition.pdf',
+  tags: ['Area Control', 'Tile Placement'],
+  owner: 'Dave'
+}, {
+  name: 'Forbidden Island',
+  description: 'Forbidden Island was the secluded retreat of an ancient mystical empire known as the Archeans. Legend has it that the Archeans possessed the ability to control the Earth’s core elements – fire, wind, water, and earth – through four sacred treasures: The Crystal of Fire, The Statue of  the Wind, The Ocean’s Chalice, and The Earth Stone. Because of their potential to cause catastrophic damage if they fell into enemy hands, the Archeans kept the treasures secretly hidden on Forbidden Island and designed it to sink if intruders ever attempted to claim them. In the centuries since the mysterious collapse of their empire, Forbidden Island remained undiscovered... until now.',
+  playersMin: 2,
+  playersMax: 4,
+  playTimeQuick: 25,
+  playTimeLong: 30,
+  rulesPDF: 'http://www.gamewright.com/gamewright/pdfs/Rules/ForbiddenIslandTM-RULES.pdf',
+  tags: ['Hand Management', 'Set Collection', 'Co-op'],
+  owner: 'Dave'
+}, {
+  name: 'Codenames',
+  description: 'In Codenames, two teams compete to see who can make contact with all of their agents first. Spymasters give one-word clues that can point to multiple words on the board. Their teammates try to guess words of the right color while avoiding those that belong to the opposing team. And everyone wants to avoid the assassin.',
+  playersMin: 2,
+  playersMax: 8,
+  playTimeQuick: 10,
+  playTimeLong: 15,
+  rulesPDF: 'http://czechgames.com/files/rules/codenames-rules-en.pdf',
+  tags: ['Memory', 'Press Your Luck', 'Party Game'],
+  owner: 'Dave'
+}, {
+  name: 'Alchemists',
+  description: 'In Alchemists, two to four budding alchemists compete to discover the secrets of their mystical art. Points can be earned in various ways, but most points are earned by publishing theories – correct theories, that is — and therein lies the problem.',
+  playersMin: 2,
+  playersMax: 4,
+  playTimeQuick: 120,
+  playTimeLong: 180,
+  rulesPDF: 'http://czechgames.com/files/rules/alchemists-rules-en.pdf',
+  tags: ['Card Drafting', 'Hand Management', 'Worker Placement'],
+  owner: 'Dave'
+}, {
+  name: 'Battlestar Galactica',
+  description: 'Battlestar Galactica: The Board Game provides a unique gaming experience. Unlike most games where players win individually, Battlestar Galactica is a team game, with the added complication that which players are on which teams is kept secret. Each player is secretly assigned a team at the start of the game. The two teams are the humans and the Cylons, and each team has a specific objective. The human players are trying to find the map to Earth, while the Cylon players simply want to annihilate the human race. Players win or lose with the other members of their team...but must figure out who they can trust in order to lead their team to victory.',
+  playersMin: 3,
+  playersMax: 6,
+  playTimeQuick: 120,
+  playTimeLong: 240,
+  rulesPDF: 'https://images-cdn.fantasyflightgames.com/filer_public/0c/0c/0c0cc32c-6394-448c-b5c0-9307f85dfc74/bsg_rulebook_web.pdf',
+  tags: ['Area Movement', 'Bluffing', 'Dice Rolling', 'Hand Management', 'Role Playing'],
+  owner: 'Dave'
+}, {
+  name: 'Cosmic Encounter',
+  description: 'Build a galactic empire... In the depths of space, the alien races of the Cosmos vie with each other for control of the universe. Alliances form and shift from moment to moment, while cataclysmic battles send starships screaming into the warp. Players choose from dozens of alien races, each with its own unique power to further its efforts to build an empire that spans the galaxy.',
+  playersMin: 3,
+  playersMax: 5,
+  playTimeQuick: 60,
+  playTimeLong: 120,
+  rulesPDF: 'https://images-cdn.fantasyflightgames.com/filer_public/11/c6/11c61988-bb60-428f-b614-9c3a952f070b/cosmic-encounter-rulebook.pdf',
+  tags: ['Bluffing', 'Hand Management', 'Take That'],
   owner: 'Dave'
 }];
 
